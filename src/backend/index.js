d28a9ce0bd3228532f02fc1069e6e0e189b5c0e4
Jhonatan Frade <jhonatanfrade.pereira@gmail.com>
2020-05-09 15:15:45 -0300

    Paths to Rows

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,11 +1,17 @@
 const { ipcMain } = require('electron')
+const pathsToRows = require('./pathsToRows')
 
 ipcMain.on('process-subtitles', (event, paths) => {
     console.log(paths)
 
-    event.reply('process-subtitles', [
-        { name: 'i', amount: 1250 },
-        { name: 'you', amount: 900 },
-        { name: 'he', amount: 800 },
-    ])
+    pathsToRows(paths)
+        .then(rows => console.log(rows))
+        .then(() => {
+            event.reply('process-subtitles', [
+                { name: 'i', amount: 1250 },
+                { name: 'you', amount: 900 },
+                { name: 'he', amount: 800 },
+            ])
+        })
+
 })
